9ca1debad9bb5c92ffe1cbd4f7c3c5d2b04da7db
chechoEducativo <156550492+chechoEducativo@users.noreply.github.com>
2024-08-14 15:34:59 -0500

    updated gitignore for pycharm

--- src/sdf_baker.py
+++ src/sdf_baker.py
@@ -2,6 +2,7 @@ import bpy
 import numpy as np
 import os
 
+
 def bake_sdf_3d_from_mesh(path_to_3d_model: str) -> np.ndarray:
     bpy.ops.import_scene.fbx(filepath=path_to_3d_model)
     model_name = os.path.splitext(os.path.basename(path_to_3d_model))[0]
